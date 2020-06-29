b8e2c13e51ef35dd71101faa5c435666f4d116d0
Andreas Karabetian <63111742+adreaskar@users.noreply.github.com>
2020-06-29 21:21:16 +0000

    Add files via upload

---
+++ Project/flask/static/fun.js
@@ -0,0 +1,12 @@
+
+function showForm() {
+    document.getElementById("movie_update_row").style.display="block";
+    
+    document.getElementById("title_update").style.display="block";
+    document.getElementById("year_update").style.display="block";
+    document.getElementById("plot_update").style.display="block";
+    document.getElementById("actors_update").style.display="block";
+    document.getElementById("actors_remove").style.display="block";
+
+    document.getElementById("movie_update_submit").style.display="block";
+}
